abedbacf9e7c742fb99ce6e817d2eddb7e5a7b15
Hideto Yasunori <hideto0118yasunori@gmail.com>
2016-09-28 16:16:55 -0700

    Add column post_id to Comments Table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160928231041) do
+ActiveRecord::Schema.define(version: 20160928231517) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content",            null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160928231041) do
     t.integer  "lecture_id"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.integer  "post_id"
   end
 
   create_table "courses", force: :cascade do |t|
