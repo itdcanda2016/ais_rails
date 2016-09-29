449bb21141aa499a40024c7d5ff2fe50ba8aad8e
Hideto Yasunori <hideto0118yasunori@gmail.com>
2016-09-28 18:15:56 -0700

    Make static home page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,2 @@
 class PostsController < ApplicationController
-
-  def home
-    @posts = Post.all.order("created_at DESC").limit(3)
-  end
-
 end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+
+  def index
+    @posts = Post.all.order("created_at DESC").limit(3)
+  end
+
+end
